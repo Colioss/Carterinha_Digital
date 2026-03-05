9846431289236ee3fa69d750495c74332f52e803
Aluno <manu.ajo08@gmail.com>
2026-03-05 16:40:31 -0300

    oi

--- app/src/main/java/com/senai/carterinhadigital/MainActivity.kt
+++ app/src/main/java/com/senai/carterinhadigital/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.senai.carterinhadigital.ui.theme.CarterinhaDigitalTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +26,35 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CarterinhaDigitalTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Scaffold{
+                    innerPadding ->
+                    CarterinhaDigitalApp(
+                        modifier = Modifier
+                            .padding( paddingValues = innerPadding)
+                            .fillMaxSize()
                     )
                 }
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    CarterinhaDigitalTheme {
-        Greeting("Android")
+fun CarterinhaDigitalApp(modifier: Modifier = Modifier) {
+    Column(
+        modifier=modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(200.dp)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.img),
+            contentDescription = null
+        )
+        Image(
+            painter = painterResource(R.drawable.perfil),
+            contentDescription = null
+        )
     }
 }
