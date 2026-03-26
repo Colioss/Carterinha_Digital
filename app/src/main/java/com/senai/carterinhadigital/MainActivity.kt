e05ce88799e9989a75d6dd5ab1ca13bbd24b216c
Julia Mendes <cordeiroj647@gmail.com>
2026-03-26 16:53:37 -0300

    oi

--- app/src/main/java/com/senai/carterinhadigital/MainActivity.kt
+++ app/src/main/java/com/senai/carterinhadigital/MainActivity.kt
@@ -5,19 +5,28 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.rafaelcosta.carteirinhadigital_4devm_t1.feature.carteirinha.presentation.component.QrCode
 import com.senai.carterinhadigital.ui.theme.CarterinhaDigitalTheme
 
 class MainActivity : ComponentActivity() {
@@ -43,18 +52,70 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CarterinhaDigitalApp(modifier: Modifier = Modifier) {
+    Box{
+        Image(
+            painter = painterResource(id = R.drawable.fundo),
+            contentDescription = "Fundo",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+
+        )
     Column(
         modifier=modifier,
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(200.dp)
+        verticalArrangement = Arrangement.spacedBy(20.dp)
     ) {
         Image(
             painter = painterResource(R.drawable.img),
-            contentDescription = null
+            contentDescription = "Aluno",
+            modifier = Modifier
+                .size(200.dp)
+                .clip(CircleShape)
+                .border(
+                    width = 2.dp,
+                    Color.Red
+                )
         )
         Image(
-            painter = painterResource(R.drawable.perfil),
+            painter = painterResource(R.drawable.pantera),
             contentDescription = null
         )
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Text(text = "Nome:")
+            Text(text = "Emanu")
+        }
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Text(text = "Curso:")
+            Text(text = "Desenvolvimento De Sistemas")
+        }
+
+        QrCode(
+            conteudo = "90000000001771684443",
+            modifier = Modifier
+                .border(
+                    width = 2.dp,
+                    Color.Red
+                )
+        )
+      }
     }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun CarterinhaDigitalAppPreview() {
+    CarterinhaDigitalTheme{
+        CarterinhaDigitalApp(
+            modifier = Modifier.fillMaxSize()
+        )
+
+   }
 }
